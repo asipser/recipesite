9997c97d594493db4bd522ebcc80a3697492707a
Aaron Sipser <ajsipser@gmail.com>
2020-06-29 20:50:31 -0400

    first commit

---
+++ server/db.js
@@ -0,0 +1,24 @@
+const { Pool } = require("pg");
+const logger = require("pino")(); // import pino logger
+
+let pool;
+
+module.exports = {
+  pool,
+  init: () => {
+    const connectionString = process.env.POSTGRES_CONNECTION_URI;
+    pool = new Pool({connectionString});
+    pool.connect((err, client, done) => {
+      if (err) logger.error("Error connecting to Postgres", err);
+      else logger.info("Server connected to Postgres");
+      done(err);
+    });
+    pool.on("error", function(err) {
+      logger.error("Postgres err: ", err);
+    });
+  },
+  query: (text, params) => {
+    return pool.query(text, params);
+  },
+};
+
