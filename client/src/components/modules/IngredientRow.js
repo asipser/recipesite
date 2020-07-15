735681678e23a1f3b9a36354d9755c5a4dd70516
Daniel <danifili96@gmail.com>
2020-07-15 17:53:36 -0400

    refactored ingredient row

--- client/src/components/modules/IngredientRow.js
+++ client/src/components/modules/IngredientRow.js
@@ -2,60 +2,60 @@ import React, { useRef, useState, useEffect } from "react";
 import EditableText from "./EditableText";
 import "./IngredientRow.css";
 
+const getAutofill = (options, text) => {
+  if (!options.includes(text)) {
+    const autofillOptions = options.filter((option) => {
+      return option.startsWith(text);
+    });
+    if (autofillOptions.length > 0) {
+      return autofillOptions[0];
+    } else {
+      return options[0];
+    }
+  } else {
+    return text;
+  }
+};
+
+const getCellWithFilled = (cell, filled) => {
+  const newCell = { ...cell };
+  newCell.filled = filled;
+  return newCell;
+};
+
+const getCellWithText = (cell, text) => {
+  const newCell = { ...cell };
+  newCell.text = text;
+  return newCell;
+};
+
 const IngredientRow = ({ rowNumber, cellsMetadata, onFilledRow, rowContent }) => {
   const generateRowCell = (textField, filled) => {
-    return { text: textField, filled: filled || textField };
+    return { text: textField, filled: filled || !!textField };
   };
 
   const units = ["cup", "ounce", "pound"];
+  const stores = ["trader joe's", "whole's foods"];
 
-  const [rowCells, setRowCells] = useState([
-    generateRowCell(rowContent.amount, rowContent.filled),
-    generateRowCell(rowContent.unit, rowContent.filled),
-    generateRowCell(rowContent.item, rowContent.filled),
-    generateRowCell(rowContent.store, rowContent.filled),
-  ]);
-
+  const [cellAmount, setCellAmount] = useState(
+    generateRowCell(rowContent.amount, rowContent.filled)
+  );
+  const [cellUnit, setCellUnit] = useState(generateRowCell(rowContent.unit, rowContent.filled));
+  const [cellItem, setCellItem] = useState(generateRowCell(rowContent.item, rowContent.filled));
+  const [cellStore, setCellStore] = useState(generateRowCell(rowContent.store, rowContent.filled));
   const [checkPantry, setCheckPantry] = useState(rowContent.checkPantry);
-
-  const handleChange = (colNumber, event) => {
-    const rowCellsCopy = [...rowCells];
-    rowCellsCopy[colNumber].text = event.target.value;
-    setRowCells(rowCellsCopy);
-  };
 
   const handleCheckbox = (e) => {
     setCheckPantry(!checkPantry);
   };
 
-  const handleOnEditCell = (colNumber) => {
-    const rowCellsCopy = [...rowCells];
-    rowCellsCopy[colNumber].filled = true;
-    setRowCells(rowCellsCopy);
-  };
-
-  const handleOnEditUnitCell = () => {
-    const rowCellsCopy = [...rowCells];
-    if (!units.includes(rowCellsCopy[1].text)) {
-      const autofillOptions = units.filter((unit) => {
-        return unit.startsWith(rowCellsCopy[1].text);
-      });
-      if (autofillOptions.length > 0) {
-        rowCellsCopy[1].text = autofillOptions[0];
-      } else {
-        rowCellsCopy[1].text = units[0];
-      }
-    }
-    rowCellsCopy[1].filled = true;
-    setRowCells(rowCellsCopy);
-  };
-
   useEffect(() => {
-    document.getElementById(`input-${rowNumber}-${0}`).focus();
+    document.getElementById(`input-${rowNumber}-Amount$`).focus();
   }, []);
 
   useEffect(() => {
     let rowFilled = true;
+    const rowCells = [cellAmount, cellUnit, cellItem, cellStore];
     for (let i = 0; i < rowCells.length; i++) {
       if (!rowCells[i].filled) {
         rowFilled = false;
@@ -63,98 +63,22 @@ const IngredientRow = ({ rowNumber, cellsMetadata, onFilledRow, rowContent }) =>
     }
     if (rowFilled) {
       onFilledRow({
-        amount: rowCells[0].text,
-        unit: rowCells[1].text,
-        item: rowCells[2].text,
-        store: rowCells[3].text,
+        amount: cellAmount.text,
+        unit: cellUnit.text,
+        item: cellItem.text,
+        store: cellStore.text,
         checkPantry: checkPantry,
       });
     }
-  }, [rowCells, checkPantry]);
-
-  const refs = [useRef(), useRef(), useRef(), useRef()];
+  }, [cellAmount, cellUnit, cellItem, cellStore, checkPantry]);
 
   return (
     <div className={"IngredientRow-Container"}>
       <form className={"IngredientRow-Form"}>
-        <div>
-          <EditableText
-            divClassName={`${cellsMetadata[0].divClassName} IngredientRow-Cell`}
-            text={rowCells[0].text}
-            childRef={refs[0]}
-            placeholder={cellsMetadata[0].placeholder}
-            onFinishedEditing={() => handleOnEditCell(0)}
-          >
-            <input
-              className={"IngredientRow-Input"}
-              ref={refs[0]}
-              id={`input-${rowNumber}-${0}`}
-              value={rowCells[0].text}
-              onChange={(event) => handleChange(0, event)}
-              placeholder={cellsMetadata[0].placeholder}
-            />
-          </EditableText>
-        </div>
-        <div>
-          <EditableText
-            divClassName={`${cellsMetadata[1].divClassName} IngredientRow-Cell`}
-            text={rowCells[1].text}
-            childRef={refs[1]}
-            placeholder={cellsMetadata[1].placeholder}
-            onFinishedEditing={handleOnEditUnitCell}
-          >
-            <input
-              list={`units-${rowNumber}`}
-              className={"IngredientRow-Input"}
-              ref={refs[1]}
-              id={`input-${rowNumber}-${1}`}
-              value={rowCells[1].text}
-              onChange={(event) => handleChange(1, event)}
-              placeholder={cellsMetadata[1].placeholder}
-            />
-            <datalist id={`units-${rowNumber}`}>
-              {units.map((unit) => (
-                <option key={`unit-${rowNumber}-${unit}`} value={unit} />
-              ))}
-            </datalist>
-          </EditableText>
-        </div>
-        <div>
-          <EditableText
-            divClassName={`${cellsMetadata[2].divClassName} IngredientRow-Cell`}
-            text={rowCells[2].text}
-            childRef={refs[2]}
-            placeholder={cellsMetadata[2].placeholder}
-            onFinishedEditing={() => handleOnEditCell(2)}
-          >
-            <input
-              className={"IngredientRow-Input"}
-              ref={refs[2]}
-              id={`input-${rowNumber}-${2}`}
-              value={rowCells[2].text}
-              onChange={(event) => handleChange(2, event)}
-              placeholder={cellsMetadata[2].placeholder}
-            />
-          </EditableText>
-        </div>
-        <div>
-          <EditableText
-            divClassName={`${cellsMetadata[3].divClassName} IngredientRow-Cell`}
-            text={rowCells[3].text}
-            childRef={refs[3]}
-            placeholder={cellsMetadata[3].placeholder}
-            onFinishedEditing={() => handleOnEditCell(3)}
-          >
-            <input
-              className={"IngredientRow-Input"}
-              ref={refs[3]}
-              id={`input-${rowNumber}-${3}`}
-              value={rowCells[3].text}
-              onChange={(event) => handleChange(3, event)}
-              placeholder={cellsMetadata[3].placeholder}
-            />
-          </EditableText>
-        </div>
+        <CellAmount cell={cellAmount} setCell={setCellAmount} rowNumber={rowNumber} />
+        <CellUnit cell={cellUnit} setCell={setCellUnit} rowNumber={rowNumber} units={units} />
+        <CellItem cell={cellItem} setCell={setCellItem} rowNumber={rowNumber} />
+        <CellStore cell={cellStore} setCell={setCellStore} rowNumber={rowNumber} stores={stores} />
         <div style={{ marginRight: "8px" }}>Check Pantry:</div>
         <div>
           <input
@@ -166,6 +90,148 @@ const IngredientRow = ({ rowNumber, cellsMetadata, onFilledRow, rowContent }) =>
         </div>
       </form>
     </div>
+  );
+};
+
+const CellAmount = ({ rowNumber, cell, setCell }) => {
+  const placeholder = "Amount";
+  const inputRef = useRef();
+  const text = cell.text;
+  const setText = (newText) => {
+    setCell(getCellWithText(cell, newText));
+  };
+  const setFilled = () => {
+    setCell(getCellWithFilled(cell, true));
+  };
+  return (
+    <EditableText
+      divClassName={`IngredientRow-Cell-Amount IngredientRow-Cell`}
+      text={text}
+      childRef={inputRef}
+      placeholder={placeholder}
+      onFinishedEditing={setFilled}
+    >
+      <input
+        className={"IngredientRow-Input"}
+        type={"number"}
+        ref={inputRef}
+        id={`input-${rowNumber}-Amount$`}
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        placeholder={placeholder}
+      />
+    </EditableText>
+  );
+};
+
+const CellUnit = ({ units, rowNumber, cell, setCell }) => {
+  const placeholder = "Unit";
+  const inputRef = useRef();
+  const text = cell.text;
+  const setText = (newText) => {
+    setCell(getCellWithText(cell, newText));
+  };
+  const handleFinishedEditing = () => {
+    const newText = getAutofill(units, text);
+    setCell({
+      text: newText,
+      filled: true,
+    });
+  };
+
+  return (
+    <EditableText
+      divClassName={`IngredientRow-Cell-Unit IngredientRow-Cell`}
+      text={text}
+      childRef={inputRef}
+      placeholder={placeholder}
+      onFinishedEditing={handleFinishedEditing}
+    >
+      <input
+        list={`units-${rowNumber}`}
+        className={"IngredientRow-Input"}
+        ref={inputRef}
+        id={`input-${rowNumber}-Unit`}
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        placeholder={placeholder}
+      />
+      <datalist id={`units-${rowNumber}`}>
+        {units.map((unit) => (
+          <option key={`unit-${rowNumber}-${unit}`} value={unit} />
+        ))}
+      </datalist>
+    </EditableText>
+  );
+};
+
+const CellItem = ({ cell, setCell, rowNumber }) => {
+  const placeholder = "Item";
+  const inputRef = useRef();
+  const text = cell.text;
+  const setText = (newText) => {
+    setCell(getCellWithText(cell, newText));
+  };
+  const setFilled = () => {
+    setCell(getCellWithFilled(cell, true));
+  };
+  return (
+    <EditableText
+      divClassName={`IngredientRow-Cell-Item IngredientRow-Cell`}
+      text={text}
+      childRef={inputRef}
+      placeholder={placeholder}
+      onFinishedEditing={setFilled}
+    >
+      <input
+        className={"IngredientRow-Input"}
+        ref={inputRef}
+        id={`input-${rowNumber}-Item`}
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        placeholder={placeholder}
+      />
+    </EditableText>
+  );
+};
+
+const CellStore = ({ cell, setCell, rowNumber, stores }) => {
+  const placeholder = "Store";
+  const inputRef = useRef();
+  const text = cell.text;
+  const setText = (newText) => {
+    setCell(getCellWithText(cell, newText));
+  };
+  const handleFinishedEditing = () => {
+    const newText = getAutofill(stores, text);
+    setCell({
+      text: newText,
+      filled: true,
+    });
+  };
+  return (
+    <EditableText
+      divClassName={`IngredientRow-Cell-Store IngredientRow-Cell`}
+      text={text}
+      childRef={inputRef}
+      placeholder={placeholder}
+      onFinishedEditing={handleFinishedEditing}
+    >
+      <input
+        list={`stores-${rowNumber}`}
+        className={"IngredientRow-Input"}
+        ref={inputRef}
+        id={`input-${rowNumber}-Store`}
+        value={text}
+        onChange={(event) => setText(event.target.value)}
+        placeholder={placeholder}
+      />
+      <datalist id={`stores-${rowNumber}`}>
+        {stores.map((store) => (
+          <option key={`store-${rowNumber}-${store}`} value={store} />
+        ))}
+      </datalist>
+    </EditableText>
   );
 };
 
