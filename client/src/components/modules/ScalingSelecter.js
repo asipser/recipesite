8c4e22fcf74059cb7307a53b0c1d18f22dc67586
Daniel <danifili96@gmail.com>
2020-07-26 21:25:29 -0400

    update selectedRecipes state of frontend

---
+++ client/src/components/modules/ScalingSelecter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ScalingSelecter = ({ recipes }) => {
+  return <div></div>;
+};
+
+export default ScalingSelecter;
--- client/src/components/pages/ShoppingList.js
+++ client/src/components/pages/ShoppingList.js
@@ -1,32 +1,46 @@
 import React, { useState, useEffect } from "react";
 import PantrySelecter from "../modules/PantrySelecter";
+import ScalingSelecter from "../modules/ScalingSelecter";
 import { get, getRawShoppingList, setShoppingList } from "../../utilities";
+import "./ShoppingList.css";
 
 const ShoppingList = () => {
   const [selectedRecipes, setSelectedRecipes] = useState([]);
   const [pantryIngredientsMap, setPantryIngredientsMap] = useState({});
+  const [viewPantry, setViewPantry] = useState(true);
 
-  const getSelectedRecipes = () => {
+  const getSelectedRecipeNames = () => {
     const recipeMap = getRawShoppingList();
     return Object.keys(recipeMap).filter((recipe) => recipeMap[recipe]);
   };
 
-  const updatePantryIngredients = (newSelectedRecipes) => {
-    get("/api/shopping", { recipes: newSelectedRecipes }).then(({ pantryIngredients }) => {
-      const newPantryIngredientsMap = {};
-      pantryIngredients.forEach((ingredient) => {
-        newPantryIngredientsMap[ingredient] = false;
-      });
-      console.log(newPantryIngredientsMap);
-      setPantryIngredientsMap(newPantryIngredientsMap);
-    });
+  const fetchData = async (selectedRecipeNames) => {
+    return get("/api/shopping", { recipes: selectedRecipeNames }).then(
+      ({ pantryIngredients, recipes }) => {
+        const newPantryIngredientsMap = {};
+        pantryIngredients.forEach((ingredient) => {
+          newPantryIngredientsMap[ingredient] =
+            pantryIngredientsMap[ingredient] !== undefined
+              ? pantryIngredientsMap[ingredient]
+              : false;
+        });
+        return {
+          pantryIngredientsMap: newPantryIngredientsMap,
+          recipes: recipes,
+        };
+      }
+    );
   };
 
   const handleRemoveRecipe = (recipeName) => {
     setShoppingList(recipeName, false);
-    const newSelectedRecipes = selectedRecipes.filter((otherRecipe) => otherRecipe !== recipeName);
-    setSelectedRecipes(newSelectedRecipes);
-    updatePantryIngredients(newSelectedRecipes);
+    const newSelectedRecipes = selectedRecipes.filter(
+      (otherRecipe) => otherRecipe.name !== recipeName
+    );
+    fetchData(newSelectedRecipes.map((recipe) => recipe.name)).then((data) => {
+      setPantryIngredientsMap(data.pantryIngredientsMap);
+      setSelectedRecipes(newSelectedRecipes);
+    });
   };
 
   const handleOnToggle = (ingredient, addToList) => {
@@ -34,18 +48,33 @@ const ShoppingList = () => {
   };
 
   useEffect(() => {
-    const newSelectedRecipes = getSelectedRecipes();
-    setSelectedRecipes(newSelectedRecipes);
-    updatePantryIngredients(newSelectedRecipes);
+    const newSelectedRecipeNames = getSelectedRecipeNames();
+    fetchData(newSelectedRecipeNames).then((data) => {
+      setPantryIngredientsMap(data.pantryIngredientsMap);
+      setSelectedRecipes(
+        data.recipes.map((recipe) => ({ ...recipe, userServings: recipe.servings }))
+      );
+    });
   }, []);
 
   return (
-    <PantrySelecter
-      selectedRecipes={selectedRecipes}
-      pantryIngredientsMap={pantryIngredientsMap}
-      onRemoveRecipe={handleRemoveRecipe}
-      onToggleIngredient={handleOnToggle}
-    />
+    <div className="ShoppingList-Container">
+      <div className="ShoopingList-Body">
+        {viewPantry ? (
+          <PantrySelecter
+            selectedRecipes={selectedRecipes.map((recipe) => recipe.name)}
+            pantryIngredientsMap={pantryIngredientsMap}
+            onRemoveRecipe={handleRemoveRecipe}
+            onToggleIngredient={handleOnToggle}
+          />
+        ) : (
+          <ScalingSelecter />
+        )}
+      </div>
+      <div className="ShoppingList-Footer">
+        <button onClick={(e) => setViewPantry(!viewPantry)}>Change</button>
+      </div>
+    </div>
   );
 };
 
